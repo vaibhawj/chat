aa4bdd7087a651552c9068176c8afcfef161dee0
Vaibhaw Jaiswal <vaibhaw@Vaibhaws-MacBook-Air.local>
2018-01-24 00:33:45 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// app setup
+var app = express();
+var server = app.listen(4000, function() {
+    console.log('Listening on port 4000');
+})
+
+// static files
+app.use(express.static('public'));
+
+// socket setup
+var io = socket(server);
+
+io.on('connection', function(socket) {
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat', function(data) {
+        io.sockets.emit('chat', data);
+    })
+
+    socket.on('typing', function(data) {
+        socket.broadcast.emit('typing', data);
+    })
+});
